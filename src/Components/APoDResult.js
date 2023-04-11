e4caa4e2373cd11af870aba61440a0e1fe5b5230
Eli Cohen <cohen.elijahlev@gmail.com>
2023-04-11 15:13:32 -0400

    reverted date formatting

--- src/Components/APoDResult.js
+++ src/Components/APoDResult.js
@@ -3,7 +3,7 @@ import React from 'react';
 const APoDResult = ({ date, explanation, media_type, title, url }) => {
 	return (
 		<div className="APoDResult">
-			<h2>Astronomy Picture of the Day for {new Date(date).toLocaleString('en-us')}</h2>
+			<h2>Astronomy Picture of the Day for {date}</h2>
 
 			<h3>{title}</h3>
 
